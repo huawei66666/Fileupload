f6b3a35954c9aab43781f18e104d4b6707c0ee56
lhw <1170137052@qq.com>
2018-07-05 18:12:17 +0800

    添加spring mvc

--- src/main/java/com/huawei/controller/UploadController.java
+++ src/main/java/com/huawei/controller/UploadController.java
@@ -4,6 +4,10 @@ import com.huawei.bean.ResourceDatabaseBo;
 import com.huawei.service.UploadService;
 import com.huawei.utils.ImportExcel;
 import com.huawei.utils.RequestUtils;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +17,11 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 import static com.sun.deploy.registration.InstallCommands.STATUS_OK;
 import static sun.plugin2.message.StartAppletAckMessage.STATUS_ERROR;
@@ -41,6 +49,68 @@ public class UploadController extends BaseController {
     public String upload() throws Exception {
         return "upload";
     }
+
+    /**
+     * 上传跳转
+     *
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "uploadTest")
+    public String uploadTest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        File file;
+
+        String savePath = request.getContextPath();
+        savePath = "" + savePath + "/uploads/";
+        File f1 = new File(savePath);
+        System.out.println(savePath);
+        if (!f1.exists()) {
+            f1.mkdirs();
+        }
+        DiskFileItemFactory fac = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(fac);
+        upload.setHeaderEncoding("utf-8");
+        List<FileItem> fileList = null;
+        try {
+            fileList = upload.parseRequest(request);
+        } catch (FileUploadException ex) {
+            throw new RuntimeException("上传异常！");
+        }
+        Iterator<FileItem> it = fileList.iterator();
+        String name = "";
+        String extName = "";
+
+        while (it.hasNext()) {
+            FileItem item = it.next();
+            if (!item.isFormField()) {
+                name = item.getName();
+                long size = item.getSize();
+                String type = item.getContentType();
+                System.out.println(size + " " + type);
+                if (name == null || name.trim().equals("")) {
+                    continue;
+                }
+                // 扩展名格式：
+                if (name.lastIndexOf(".") >= 0) {
+                    extName = name.substring(name.lastIndexOf("."));
+                }
+                do {
+                    // 生成文件名：
+                    name = UUID.randomUUID().toString();
+                    file = new File(savePath + name + extName);
+                } while (file.exists());
+                try {
+                    item.write(file);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return "upload";
+    }
+
 
     /**
      * 导入Excel
--- src/main/webapp/WEB-INF/views/upload.jsp
+++ src/main/webapp/WEB-INF/views/upload.jsp
@@ -1,11 +1,31 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ include file="/WEB-INF/include/taglib.jsp" %>
 <html>
 <head>
     <meta http-equiv="content-type" content="text/html;charset=utf-8">
     <title>带进度条上传</title>
+    <script src="${ctxStatic}/jquery-1.12.4.min.js"></script>
+    <script src="${ctxStatic}/jquery.uploadify.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="${ctxStatic}/uploadify.css" />
+
 </head>
 <body>
-<h5>哈哈哈哈哈</h5>
+<h1>上传测试</h1>
+<form>
+    <div id="queue"></div>
+    <input id="file_upload" name="file_upload" type="file" multiple="true">
+</form>
+
+<script type="text/javascript">
+    $(function () {
+        $("#file_upload").uploadify({
+            height: 30,
+            swf: '${ctxStatic}/uploadify.swf',
+            uploader: '/uploadExcel',
+            width: 120
+        });
+    });
+</script>
 
 </body>
 </html>
